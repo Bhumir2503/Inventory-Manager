4cc73f61d71584c113adf3db4b8c7507ca4a34a4
Bhumir2503 <bhumir2503@gmail.com>
2023-11-23 14:16:31 -0500

    build command

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 app.use('/Inventory-Manager/', itemRoutes)
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,  { ssl: true })
   .then(() => {
     console.log('connected to database')
     // listen to port
